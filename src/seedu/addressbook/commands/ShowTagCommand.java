2e8cffdeadb5bc5579230be15d9fd36054262b49
githubxxcc <xuchen727@hotmail.com>
2016-09-22 20:04:21 +0800

    Initial commit

---
+++ src/seedu/addressbook/commands/ShowTagCommand.java
@@ -0,0 +1,11 @@
+package seedu.addressbook.commands;
+
+public class ShowTagCommand extends Command {
+    public static final String COMMAND_WORD = "edit-tag";
+    @Override
+    public CommandResult execute() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
